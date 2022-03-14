e2e44452857c2f251a56a379f1934c78b78483af
Umme Kawser Sinthia <sinthy08@gmail.com>
2022-03-14 13:04:44 +0600

    changed the required version

--- version.php
+++ version.php
@@ -25,17 +25,9 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-// This is the version of the plugin.
-$plugin->version = '2022022400';
 
+$plugin->version = 2022031400; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE;
-// This is the version of Moodle this plugin requires.
-$plugin->requires = '2020061500';
-
-// This is the component name of the plugin - it always starts with 'theme_'
-// for themes and should be the same as the name of the folder.
+$plugin->requires  = 2020110300; // 3.10
 $plugin->component = 'theme_edubs';
-
-// This is a list of plugins, this plugin depends on (and their versions).
-$plugin->dependencies = array('theme_boost' => 2021051100);
 $plugin->release = 'v1.0.0';
